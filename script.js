fcfa7ebbb4b8bc48ed638307854b8db57909c696
jorgecp670 <124124636+jorgecp670@users.noreply.github.com>
2023-08-13 20:24:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -28,12 +28,107 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function displayCharacterSheet(formData) {
         const characterSheetContent = `
-            <!-- ... (código anterior) ... -->
-        `;
+            <div id="characterSheetContent">
+                <h2>Dados do Personagem</h2>
+                <label for="name">Nome:</label>
+                <input type="text" id="name" name="name" value="${formData.get('name')}"><br>
+    
+                <label for="characterClass">Classe:</label>
+                <input type="text" id="characterClass" name="characterClass" value="${formData.get('characterClass')}"><br>
+    
+                <label for="region">Região (Origem):</label>
+                <input type="text" id="region" name="region" value="${formData.get('region')}"><br>
+    
+                <label for="faith">Fé:</label>
+                <input type="text" id="faith" name="faith" value="${formData.get('faith')}"><br>
+    
+                <label for="background">Antecedente:</label>
+                <input type="text" id="background" name="background" value="${formData.get('background')}"><br>
+    
+                <label for="ageReal">Idade (real):</label>
+                <input type="number" id="ageReal" name="ageReal" value="${formData.get('ageReal')}"><br>
+    
+                <label for="ageApparent">Idade (aparente):</label>
+                <input type="number" id="ageApparent" name="ageApparent" value="${formData.get('ageApparent')}"><br>
+    
+                <!-- ... (outros campos de Dados do Personagem) ... -->
+    
+                <h2>Atributos</h2>
+                <p><strong>FORÇA:</strong> ${formData.get('strength')}</p>
+                <p><strong>AGILIDADE:</strong> ${formData.get('agility')}</p>
+                <p><strong>SABEDORIA:</strong> ${formData.get('wisdom')}</p>
+                <p><strong>INTELIGÊNCIA:</strong> ${formData.get('intelligence')}</p>
+                <p><strong>SORTE:</strong> ${formData.get('luck')}</p>
+                <p><strong>ADAPTABILIDADE:</strong> ${formData.get('adaptability')}</p>
+                <p><strong>CARISMA:</strong> ${formData.get('charisma')}</p>
+    
+                <h2>Habilidades</h2>
+                <p><strong>Habilidade 1:</strong> ${formData.get('ability1')}</p>
+                <p><strong>Habilidade 2:</strong> ${formData.get('ability2')}</p>
+                <p><strong>Habilidade 3:</strong> ${formData.get('ability3')}</p>
+                <p><strong>Habilidade 4:</strong> ${formData.get('ability4')}</p>
+                
+                <h2>Perícias</h2>
+                <p><strong>Perícia 1:</strong> ${formData.get('skill1')}</p>
+                <p><strong>Perícia 2:</strong> ${formData.get('skill2')}</p>
+                <p><strong>Perícia 3:</strong> ${formData.get('skill3')}</p>
+                <p><strong>Perícia 4:</strong> ${formData.get('skill4')}</p>
+                <p><strong>Perícia 5:</strong> ${formData.get('skill5')}</p>
+                <p><strong>Perícia 6:</strong> ${formData.get('skill6')}</p>
+                <p><strong>Perícia 7:</strong> ${formData.get('skill7')}</p>
+                <p><strong>Perícia 8:</strong> ${formData.get('skill8')}</p>
+                <p><strong>Perícia 9:</strong> ${formData.get('skill9')}</p>
+                <p><strong>Perícia 10:</strong> ${formData.get('skill10')}</p>
+                <!-- ... (campos de Perícias) ... -->
+    
+                
+    <h2>Itens</h2>
+    <div class="items">
+        <h3>Consumíveis:</h3>
+        <div class="item">
+            <p><strong>Consumível 1:</strong> ${formData.get('consumable1')}</p>
+        </div>
+        <div class="item">
+            <p><strong>Consumível 2:</strong> ${formData.get('consumable2')}</p>
+        </div>
+        <div class="item">
+            <p><strong>Consumível 3:</strong> ${formData.get('consumable3')}</p>
+        </div>
+    </div>
+    <div class="items">
+        <h3>Armadura:</h3>
+        <div class="item">
+            <p><strong>Nome da Armadura:</strong> ${formData.get('armorName')}</p>
+        </div>
+        <div class="item">
+            <p><strong>Defesa Proporcionada:</strong> ${formData.get('armorDefense')}</p>
+        </div>
+        <div class="item">
+            <p><strong>Resistência:</strong> ${formData.get('armorResistance')}</p>
+        </div>
+        <!-- ... (outros campos de Armadura) ... -->
+    </div>
+    <div class="items">
+        <h3>Arma:</h3>
+        <div class="item">
+            <p><strong>Nome da Arma:</strong> ${formData.get('weaponName')}</p>
+        </div>
+        <div class="item">
+            <p><strong>Nível:</strong> ${formData.get('weaponLevel')}</p>
+        </div>
+        <div class="item">
+            <p><strong>Habilidade 1:</strong> ${formData.get('weaponAbility1')}</p>
+        </div>
+        <!-- ... (outros campos de Arma) ... -->
+    </div>
 
+
+        `;
+    
         characterSheet.style.display = 'block';
         characterDetails.innerHTML = characterSheetContent;
     }
+    
 
     rollDiceButton.addEventListener('click', rollDice);
 
@@ -112,6 +207,49 @@ document.addEventListener('DOMContentLoaded', function () {
         const formData = {
             name: document.getElementById('name').value,
             characterClass: document.getElementById('characterClass').value,
+            region: document.getElementById('region').value,
+            faith: document.getElementById('faith').value,
+            background: document.getElementById('background').value,
+            ageReal: document.getElementById('ageReal').value,
+            ageApparent: document.getElementById('ageApparent').value,
+            strength: document.getElementById('strength').value,
+            agility: document.getElementById('agility').value,
+            wisdom: document.getElementById('wisdom').value,
+            intelligence: document.getElementById('intelligence').value,
+            luck: document.getElementById('luck').value,
+            adaptability: document.getElementById('adaptability').value,
+            charisma: document.getElementById('charisma').value,
+            skill1: document.getElementById('skill1').value,
+            skill2: document.getElementById('skill2').value,
+            skill3: document.getElementById('skill3').value,
+            skill4: document.getElementById('skill4').value,
+            skill5: document.getElementById('skill5').value,
+            skill6: document.getElementById('skill6').value,
+            skill7: document.getElementById('skill7').value,
+            skill8: document.getElementById('skill8').value,
+            skill9: document.getElementById('skill9').value,
+            skill10: document.getElementById('skill10').value,
+            ability1: document.getElementById('ability1').value,
+            ability2: document.getElementById('ability2').value,
+            ability3: document.getElementById('ability3').value,
+            ability4: document.getElementById('ability4').value,
+            consumable1: document.getElementById('consumable1').value,
+            consumable2: document.getElementById('consumable2').value,
+            consumable3: document.getElementById('consumable3').value,
+            armorName: document.getElementById('armorName').value,
+            armorDefense: document.getElementById('armorDefense').value,
+            armorResistance: document.getElementById('armorResistance').value,
+            armorAbility1: document.getElementById('armorAbility1').value,
+            armorAbility2: document.getElementById('armorAbility2').value,
+            armorAbility3: document.getElementById('armorAbility3').value,
+            armorDurability: document.getElementById('armorDurability').value,
+            weaponName: document.getElementById('weaponName').value,
+            weaponLevel: document.getElementById('weaponLevel').value,
+            weaponAbility1: document.getElementById('weaponAbility1').value,
+            weaponAbility2: document.getElementById('weaponAbility2').value,
+            weaponAbility3: document.getElementById('weaponAbility3').value,
+            weaponDurability: document.getElementById('weaponDurability').value,
+            weaponDamage: document.getElementById('weaponDamage').value,
             // ... outros campos da ficha de personagem
         };
         
